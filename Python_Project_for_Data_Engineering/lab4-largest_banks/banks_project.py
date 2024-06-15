ce5885d9b865e0393928ebde93a7c10dd532ec88
Hesam <hesamshayegan88@gmail.com>
2024-06-15 13:43:41 -0700

    lab5 static code analysis

--- Python_Project_for_Data_Engineering/lab4-largest_banks/banks_project.py
+++ Python_Project_for_Data_Engineering/lab4-largest_banks/banks_project.py
@@ -12,6 +12,7 @@ log_file = 'code_log.txt'
 csv_path = 'Largest_banks_data.csv'
 db_name = 'Banks.db'
 table_name = 'Largest_banks'
+exchange_values = 'exchange_rate.csv'
 
 def log_progress(message):
     ''' This function logs the mentioned message of a given stage of the
@@ -45,74 +46,81 @@ def extract(url, table_attribs):
             
 
     return df
-df = extract(url, table_attribs)
-print(df)
-
-# def transform(df):
-#     ''' This function accesses the CSV file for exchange rate
-#     information, and adds three columns to the data frame, each
-#     containing the transformed version of Market Cap column to
-#     respective currencies'''
-#     market_cap_list = df['MC_USD_Billion'].tolist()
-
-#     market_cap_EUR = [np.round(x * 0.93, 2) for x in  market_cap_list]
-#     market_cap_GBP = [np.round(x * 0.8, 2) for x in  market_cap_list]
-#     market_cap_INR = [np.round(x * 82.95, 2) for x in  market_cap_list]
-#     df.insert(2, "MC_EUR_Billion", market_cap_EUR)
-#     df.insert(3, "MC_GBP_Billion", market_cap_GBP)
-#     df.insert(4, "MC_INR_Billion", market_cap_INR)
-
-#     return df
-
-# def load_to_csv(df, csv_path):
-#     ''' This function saves the final data frame as a CSV file in
-#     the provided path. Function returns nothing.'''
-#     df.to_csv(csv_path)
-
-# def load_to_db(df, sql_connection, table_name):
-#     ''' This function saves the final data frame to a database
-#     table with the provided name. Function returns nothing.'''
-#     df.to_sql(table_name, sql_connection, if_exists='replace', index=False)
-
-# def run_query(query_statement, sql_connection):
-#     ''' This function runs the query on the database table and
-#     prints the output on the terminal. Function returns nothing. '''
-#     print(query_statement)
-#     query_output = pd.read_sql(query_statement, sql_connection)
-#     print(query_output)
-
-
-# # TESTING
-# # log the installation of the ETL process
-# log_progress('Preliminaries complete. Initiating ETL process.')
 
 # df = extract(url, table_attribs)
 
-# log_progress('Data extraction complete. Initiating Transformation process')
+def transform(df):
+    ''' This function accesses the CSV file for exchange rate
+    information, and adds three columns to the data frame, each
+    containing the transformed version of Market Cap column to
+    respective currencies'''
+    rates = pd.read_csv(exchange_values)
 
-# df = transform(df)
+    exchange_rates = rates.set_index('Currency').to_dict()['Rate']
+    
+    market_cap_list = df['MC_USD_Billion'].tolist()
 
-# log_progress('Data transformation complete. Initiating loading process')
+    market_cap_EUR = [np.round(x * exchange_rates['EUR'], 2) for x in  market_cap_list]
+    market_cap_GBP = [np.round(x * exchange_rates['GBP'], 2) for x in  market_cap_list]
+    market_cap_INR = [np.round(x * exchange_rates['INR'], 2) for x in  market_cap_list]
+    df.insert(2, "MC_EUR_Billion", market_cap_EUR)
+    df.insert(3, "MC_GBP_Billion", market_cap_GBP)
+    df.insert(4, "MC_INR_Billion", market_cap_INR)
 
-# load_to_csv(df, csv_path)
+    return df
 
-# log_progress('Data saved to CSV file')
+# transformed_df = transform(df)
+# print(transformed_df)
 
-# sql_connection = sqlite3.connect(db_name)
+def load_to_csv(df, csv_path):
+    ''' This function saves the final data frame as a CSV file in
+    the provided path. Function returns nothing.'''
+    df.to_csv(csv_path)
 
-# log_progress('SQL Connection initiated.')
+def load_to_db(df, sql_connection, table_name):
+    ''' This function saves the final data frame to a database
+    table with the provided name. Function returns nothing.'''
+    df.to_sql(table_name, sql_connection, if_exists='replace', index=False)
 
-# load_to_db(df, sql_connection, table_name)
+def run_query(query_statement, sql_connection):
+    ''' This function runs the query on the database table and
+    prints the output on the terminal. Function returns nothing. '''
+    print(query_statement)
+    query_output = pd.read_sql(query_statement, sql_connection)
+    print(query_output)
 
-# log_progress('Data loaded to Database as table. Running the query')
 
-# query_statement = f"SELECT * FROM {table_name}"
-# run_query(query_statement, sql_connection)
-# query_statement = f"SELECT AVG(MC_GBP_Billion) FROM {table_name}"
-# run_query(query_statement, sql_connection)
-# query_statement = f"SELECT Name from {table_name} LIMIT 5"
-# run_query(query_statement, sql_connection)
+# TESTING
+# log the installation of the ETL process
+log_progress('Preliminaries complete. Initiating ETL process.')
 
-# log_progress('Process Complete.')
+df = extract(url, table_attribs)
 
-# sql_connection.close()
+log_progress('Data extraction complete. Initiating Transformation process')
+
+df = transform(df)
+
+log_progress('Data transformation complete. Initiating loading process')
+
+load_to_csv(df, csv_path)
+
+log_progress('Data saved to CSV file')
+
+sql_connection = sqlite3.connect(db_name)
+
+log_progress('SQL Connection initiated.')
+
+load_to_db(df, sql_connection, table_name)
+
+log_progress('Data loaded to Database as table. Running the query')
+
+query_statement = f"SELECT * FROM {table_name}"
+run_query(query_statement, sql_connection)
+query_statement = f"SELECT AVG(MC_GBP_Billion) FROM {table_name}"
+run_query(query_statement, sql_connection)
+query_statement = f"SELECT Name from {table_name} LIMIT 5"
+run_query(query_statement, sql_connection)
+
+log_progress('Process Complete.')
+
+sql_connection.close()
---
+++ Python_Project_for_Data_Engineering/lab5-static_code_analysis/sample2.py
@@ -0,0 +1,9 @@
+
+def add(number1, number2):
+    """This module calculates the sum of two numbers and prints the result."""
+    return number1 + number2
+
+NUM1 = 4
+NUM2 = 5
+TOTAL = add(NUM1, NUM2)
+print(f"The sum of {NUM1} and {NUM2} is {TOTAL}")
