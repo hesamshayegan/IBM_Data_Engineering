714dd6c88c00e8675e9fd6c87a5ea00e38796495
Hesam <hesamshayegan88@gmail.com>
2024-07-23 18:02:26 -0700

    lab1 shell scripting

---
+++ Shell_Scripting/lab1-greet.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+echo -n "Enter Your Name: "
+
+read name
+
+echo "Welcome $name"
+
+echo -n "Congratulations! You just created and ran your first shell script "
+
+echo "using Bash On IBM Skills Network"
+
---
+++ Shell_Scripting/lab1-greetnew.sh
@@ -0,0 +1,10 @@
+#shebang directive
+#! /bin/bash
+
+echo "Enter Your First Name: \c"
+read firstname
+
+echo "Enter Your Last Name: \c"
+read lastname
+
+echo "Hello $firstname $lastname"
